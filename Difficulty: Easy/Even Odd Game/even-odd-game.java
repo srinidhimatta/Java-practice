7ddef984d81f2c1208662b10c4140ac06b3278a7
srinidhimatta <mattanidhi99@gmail.com>
2026-09-03 23:05:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    if(n%2==0){
+        System.out.println("Friend");
+        
+    }
+        else {
+            System.out.println("You");
+        }
+    
+       
+
+        sc.close();
+    }
+}
