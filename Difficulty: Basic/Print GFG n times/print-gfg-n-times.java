9d287cf4f51d0082d903aa3085f5e8a4a478ab3f
Srinidhi Matta <mattanidhi99@gmail.com>
2026-09-04 12:46:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print GFG n times/print-gfg-n-times.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        int i=1;
+        while(i<=n){
+            System.out.print("GFG ");
+            i++;
+        }
+    }
+}
